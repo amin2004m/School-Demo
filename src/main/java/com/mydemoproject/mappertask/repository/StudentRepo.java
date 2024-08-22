1813baf40c54f6e77d9faeff016232570b1ba6d3
amin2004m <aminsfsfd@gmail.com>
2024-08-22 16:34:57 +0400

    11

--- src/main/java/com/mydemoproject/mappertask/repository/StudentRepo.java
+++ src/main/java/com/mydemoproject/mappertask/repository/StudentRepo.java
@@ -2,6 +2,7 @@ package com.mydemoproject.mappertask.repository;
 
 import com.mydemoproject.mappertask.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
